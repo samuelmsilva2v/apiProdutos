6214bb83c0aad3782213a0ace23d3008810de660
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-11-14 00:06:09 -0300

    produto service

--- src/main/java/com/example/demo/services/ProdutoService.java
+++ src/main/java/com/example/demo/services/ProdutoService.java
@@ -1,8 +1,11 @@
 package com.example.demo.services;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,23 +20,65 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	public ProdutoResponseDto create(ProdutoRequestDto request) {
 		
-		var produto = new Produto();
+		var produto = modelMapper.map(request, Produto.class);
 		produto.setId(UUID.randomUUID());
+		
+		produtoRepository.save(produto);
+		
+		var response = modelMapper.map(produto, ProdutoResponseDto.class);
+		
+		return response;
+	}
+	
+	public ProdutoResponseDto update(UUID id, ProdutoRequestDto request) {
+		
+		var produto = produtoRepository.findById(id).get();
 		produto.setNome(request.getNome());
 		produto.setPreco(BigDecimal.valueOf(request.getPreco()));
 		produto.setQuantidade(request.getQuantidade());
 		
 		produtoRepository.save(produto);
 		
-		var response = new ProdutoResponseDto();
-		response.setId(produto.getId());
-		response.setNome(produto.getNome());
-		response.setPreco(produto.getPreco().doubleValue());
-		response.setQuantidade(produto.getQuantidade());
-		
+		var response = modelMapper.map(produto, ProdutoResponseDto.class);
 		
 		return response;
 	}
+	
+	public ProdutoResponseDto delete(UUID id) {
+		
+		var produto = produtoRepository.findById(id).get();
+		
+		produtoRepository.delete(produto);
+		
+		var response = modelMapper.map(produto, ProdutoResponseDto.class);
+		
+		return response;
+	}
+	
+	public List<ProdutoResponseDto> getAll() {
+		
+		var response = new ArrayList<ProdutoResponseDto>();
+		
+		for(var produto : produtoRepository.findAll()) {
+			response.add(modelMapper.map(produto, ProdutoResponseDto.class));
+		}
+		
+		return response;
+	}
+	
+	public ProdutoResponseDto getById(UUID id) {
+		
+		var produto = produtoRepository.findById(id).get();
+		
+		var response = modelMapper.map(produto, ProdutoResponseDto.class);
+		
+		return response;
+	}
+	
+	
 }
