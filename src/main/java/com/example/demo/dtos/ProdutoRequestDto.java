13729a3bd0d0664e248e78801a8cbd8fb8287ff8
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-11-13 23:24:05 -0300

    bean validation

--- src/main/java/com/example/demo/dtos/ProdutoRequestDto.java
+++ src/main/java/com/example/demo/dtos/ProdutoRequestDto.java
@@ -1,11 +1,25 @@
 package com.example.demo.dtos;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class ProdutoRequestDto {
 
+	@NotBlank(message = "Por favor, informe o nome do produto.")
+	@Length(min = 8, message = "Nome do produto inválido. Informe um nome com, pelo menos, 8 caracteres.")
 	private String nome;
+	
+	@NotNull(message = "Por favor, informe o preço do produto.")
+	@Positive(message = "Infome um preço válido.")
 	private Double preco;
+	
+	@NotNull(message = "Por favor, informe a quantidade do produto.")
+	@Min(value = 1, message = "Informe um número válido.")
 	private Integer quantidade;
 }
