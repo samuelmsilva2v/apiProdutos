d0d5c25fecc53d91835d765a302fd4660bfa5b0b
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-11-14 00:14:33 -0300

    produto controller

--- src/main/java/com/example/demo/controllers/ProdutoController.java
+++ src/main/java/com/example/demo/controllers/ProdutoController.java
@@ -1,7 +1,14 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +29,25 @@ public class ProdutoController {
 	@PostMapping
 	public ProdutoResponseDto post(@RequestBody @Valid ProdutoRequestDto request) {
 		return produtoService.create(request);
+	}
+	
+	@PutMapping("{id}")
+	public ProdutoResponseDto put(@PathVariable UUID id, @RequestBody @Valid ProdutoRequestDto request) {
+		return produtoService.update(id, request);
+	}
+	
+	@DeleteMapping("{id}")
+	public ProdutoResponseDto delete(@PathVariable UUID id) {
+		return produtoService.delete(id);
+	}
+	
+	@GetMapping
+	public List<ProdutoResponseDto> getAll() {
+		return produtoService.getAll();
+	}
+	
+	@GetMapping("{id}")
+	public ProdutoResponseDto getById(@PathVariable UUID id) {
+		return produtoService.getById(id);
 	}
 }
