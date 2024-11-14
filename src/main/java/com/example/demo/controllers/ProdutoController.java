36fcd3d6467883b1e9f8f956383d100f2ed40c82
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-11-13 22:46:10 -0300

    created produto controller and adjusted app properties

---
+++ src/main/java/com/example/demo/controllers/ProdutoController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dtos.ProdutoRequestDto;
+import com.example.demo.services.ProdutoService;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+	@Autowired
+	private ProdutoService produtoService;
+	
+	@PostMapping
+	public String post(@RequestBody ProdutoRequestDto request) {
+		return produtoService.create(request);
+	}
+}
